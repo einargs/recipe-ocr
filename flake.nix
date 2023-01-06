f6835940f333321678bd8c81429a96534ea97770
Einar Strandberg <einargs@gmail.com>
2023-01-06 15:14:37 -0800

    new cross compile attempt

--- flake.nix
+++ flake.nix
@@ -8,18 +8,18 @@
     bash-prompt = ''\[\033[1;32m\][\[\e]0;\u@\h: \w\a\]dev-shell:\w]\$\[\033[0m\] '';
   };
   outputs = { self, nixpkgs, flake-utils }:
-    flake-utils.lib.eachSystem [ "x86_64-linux" ] (system:
+    flake-utils.lib.eachSystem [ "x86_64-linux" "aarch64-linux" ] (system:
       with import nixpkgs { system = system; };
-      let
-        raw-pi = pkgsCross.aarch64-multiplatform.haskell.packages
-          .ghc925.callPackage ./server-package.nix { };
-        patched-pi = runCommand "recipe-ocr-patched" { } ''
-          set -eu
-          cp ${raw-pi}/bin/recipe-ocr $out
-          chmod +w $out
-          ${patchelf}/bin/patchelf --set-interpreter /lib/ld-linux-aarch64.so.1 --set-rpath /lib:/usr/lib $out
-          chmod -w $out
-        '';
+      let raw-pi = haskell.packages.ghc925.callPackage ./server-package.nix { };
+          patched-pi = runCommand "recipe-ocr-patched" { } ''
+            set -eu
+            ls ${raw-pi}/bin/
+            ls ${raw-pi}/bin/recipe-ocr
+            cp ${raw-pi}/bin/recipe-ocr $out
+            chmod +w $out
+            ${patchelf}/bin/patchelf --set-interpreter /lib/ld-linux-aarch64.so.1 --set-rpath /lib:/usr/lib $out
+            chmod -w $out
+          '';
       in {
         devShells.default = mkShell {
           buildInputs = [ stack haskell.compiler.ghc925 sqlite
@@ -32,6 +32,7 @@
           PINNED_NIX_PATH = nixpkgs;
           NIX_PATH="nixpkgs=${nixpkgs}";
         };
+        packages.default = raw-pi;
         packages.pi-build = patched-pi;
       }
     );
