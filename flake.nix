7996425c23e4a5bb25e3d6e9d040054d3bd9e916
Einar Strandberg <einargs@gmail.com>
2023-01-06 23:14:34 -0800

    Got the build working!

--- flake.nix
+++ flake.nix
@@ -13,9 +13,8 @@
       let raw-pi = haskell.packages.ghc925.callPackage ./server-package.nix { };
           patched-pi = runCommand "recipe-ocr-patched" { } ''
             set -eu
-            ls ${raw-pi}/bin/
-            ls ${raw-pi}/bin/recipe-ocr
-            cp ${raw-pi}/bin/recipe-ocr $out
+            ls ${raw-pi}/bin/recipe-ocr-exe
+            cp ${raw-pi}/bin/recipe-ocr-exe $out
             chmod +w $out
             ${patchelf}/bin/patchelf --set-interpreter /lib/ld-linux-aarch64.so.1 --set-rpath /lib:/usr/lib $out
             chmod -w $out
