0015aa9b2abf739010ba15e98a2cb590c55924ad
Einar Strandberg <einargs@gmail.com>
2023-01-07 19:13:17 -0800

    Static compilation attempt

--- flake.nix
+++ flake.nix
@@ -10,7 +10,7 @@
   outputs = { self, nixpkgs, flake-utils }:
     flake-utils.lib.eachSystem [ "x86_64-linux" "aarch64-linux" ] (system:
       with import nixpkgs { system = system; };
-      let raw-pi = haskell.packages.ghc925.callPackage ./server-package.nix { };
+      let raw-pi = pkgsStatic.haskell.packages.ghc925.callPackage ./server-package.nix { };
           patched-pi = runCommand "recipe-ocr-patched" { } ''
             set -eu
             ls ${raw-pi}/bin/recipe-ocr-exe
